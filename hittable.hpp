7b65e750e328741c2954b86d92367cbfc7a7ee21
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-03-27 22:34:00 -0700

    6.3 abstraction of hittable

---
+++ hittable.hpp
@@ -0,0 +1,19 @@
+#ifndef HITTABLE_hpp
+#define HITTABLE_hpp
+
+#include "ray.hpp"
+
+struct hit_record
+{
+    point3 p;
+    vec3 normal;
+    double t;
+};
+
+class hittable
+{
+public:
+    virtual bool hit(const ray &r, double t_min, double t_max, hit_record &rec) const = 0;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -6,23 +6,32 @@ static const color WHITE_COLOR = color(1.0, 1.0, 1.0);
 static const color SKY_BLUE_COLOR = color(0.5, 0.7, 1.0);
 static const color RED_COLOR = color(1.0, 0, 0);
 
-bool hit_sphere(const point3 &center, double radius, const ray &r)
+double hit_sphere(const point3 &center, double radius, const ray &r)
 {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-    return discriminant > 0;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0) {
+        return -1;
+    } else {
+        return (-half_b - sqrt(discriminant)) / a;
+    }
+    
 }
 
 color ray_color(const ray &r)
-{   
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return RED_COLOR;
+{
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * WHITE_COLOR + t * SKY_BLUE_COLOR;
 }
 
