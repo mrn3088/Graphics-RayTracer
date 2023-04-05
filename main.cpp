376a681781aa88cf77a16777f6b1b174e9692e51
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-04-05 01:26:08 -0700

    normals-colored sphere with ground

--- main.cpp
+++ main.cpp
@@ -20,22 +20,6 @@ double hit_sphere(const point3& center, double radius, const ray& r)
     } else {
         return (-half_b - sqrt(discriminant)) / a;
     }
-
-}
-
-color ray_color(const ray& r)
-{
-
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0)
-    {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-    }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * WHITE_COLOR + t * SKY_BLUE_COLOR;
 }
 
 color ray_color(const ray& r, const hittable& world) {
