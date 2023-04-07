4406137d51e9815322b34b3aeb879765e67c3a48
Hexachlorocyclohexane3088 <97484148+Hexachlorocyclohexane3088@users.noreply.github.com>
2023-04-07 00:24:48 -0700

    defocus blur

--- main.cpp
+++ main.cpp
@@ -71,16 +71,20 @@ int main()
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
-    
+
     // Camera parameters:
     // point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio
+    point3 lookfrom(3, 3, 2);
+    point3 lookat(0, 0, -1);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = (lookfrom - lookat).length();
+    auto aperture = 2.0;
 
     // zoom out
     // camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 90, aspect_ratio); 
 
     // zoom in
-    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
-
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
     // Render
     std::cout << "P3\n"
         << image_width << " " << image_height << "\n256\n";
--- vec3.hpp
+++ vec3.hpp
@@ -156,6 +156,14 @@ vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
     return r_out_prep + r_out_parallel;
 }
 
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
 using point3 = vec3;
 using color = vec3;
 
